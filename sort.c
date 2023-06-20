2eba76b90163a204c8e8a9cff8c8088b4466a7b9
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2023-06-19 21:57:17 -0400

    implement `utoa`

--- sort.c
+++ sort.c
@@ -8,11 +8,29 @@
 
 typedef unsigned int uint;
 
-/*Return the unsigned absolute value of X.*/
+//Return the unsigned absolute value of X.
 uint uabs(const int in){
     const uint out = (uint)in;
     return in < 0 ? -out : out;
 }
+
+//Convert an unsigned integer to a decimal string.
+char* utoa(uint n)
+{
+    if (n == 0u)
+        return "0";
+    u_int8_t i;
+    //`(1<<32)-1` has 10 digits.
+    //this will sometimes be a negligible "soft" memory-leak.
+    char s[11u];
+    while (n > 0u)
+    {
+        s[i++] = '0' + (n % 10u);
+        n /= 10u;
+    };
+    s[i]='\0';
+    return s;
+};
 
 // this should be the path to the sleep binary
 #define SLEEP_PATH "/root/bruh/sleep"
@@ -20,14 +38,14 @@ uint uabs(const int in){
 int main(int argc, char** argv)
 {
     const uint n = (uint)argc;
-    if(n < 2)
+    if(n < 2u)
     {
         fprintf(stderr, "bruh");
         exit(EXIT_FAILURE);
     }
     printf("unsorted array:\n");
     int min = atoi(argv[1]);
-    const arr_len = n - 1;
+    const uint arr_len = n - 1u;
     uint *arr = malloc(arr_len * sizeof(uint));
     for(uint i = 1; i < n; i++)
     {
@@ -49,7 +67,7 @@ int main(int argc, char** argv)
                 fprintf(stderr, "fork failed");
                 exit(EXIT_FAILURE);
             case 0:
-                execl(SLEEP_PATH, "sleep", itoa(arr[i] + offset), NULL);
+                execl(SLEEP_PATH, "sleep", utoa(arr[i] + offset), NULL);
         }
     }
     free(arr);
